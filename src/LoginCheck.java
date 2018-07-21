420a2e610cf1a1d92c2e4d31c120a715cda056ea
shobinsukumaran <shobin.rs@gmail.com>
2018-07-21 13:33:02 +0530

    Add new project

---
+++ src/LoginCheck.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginCheck
+ */
+@WebServlet("/LoginCheck")
+public class LoginCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCheck() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String uname = request.getParameter("username");
+		String password= request.getParameter("password");
+		if(uname.equals("shobin") && password.equals("1234")) {
+			response.sendRedirect("success.jsp");
+		}else {
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
